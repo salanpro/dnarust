c0a585afb071e4517b3c089bdf5db1089f60232d
Sergio Alan <sergioalanpro@hotmail.com>
2025-11-12 15:13:32 -0600

    Update example_fastaWrite.rs

--- examples/example_fastaWrite.rs
+++ examples/example_fastaWrite.rs
@@ -8,7 +8,7 @@ fn main() -> std::io::Result<()> {
     let record1 = fasta::FastaRecord::new("ID_1".to_string(), seq.clone());
     let record2 = fasta::FastaRecord::new_wd("ID_2".to_string(), "Desc_2".to_string(), seq2.clone());
 
-    fasta::write_record("ouput1.fasta.fasta", &record1)?;
+    fasta::write_record("ouput1.fasta", &record1)?;
     fasta::write_record("ouput2.fasta", &record2)?;
     fasta::write_records("ouput3.fasta", &[record1, record2],
     )?;
